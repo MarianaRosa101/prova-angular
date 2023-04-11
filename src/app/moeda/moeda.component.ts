8031cdb80bb66a43c511cae6484b0551163b521e
MarianaRosa101 <marianarosa100@gmail.com>
2023-04-11 09:30:27 -0300

    third commit

--- src/app/moeda/moeda.component.ts
+++ src/app/moeda/moeda.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class MoedaComponent {
 
+  inputValue: number = 0;
+  fromUnit: string = 'R$';
+  toUnit: string = 'R$';
+  convertedValue: number = 0;
+
+  convert() {
+    let valueInR$ = 0;
+
+    switch(this.fromUnit) {
+      case 'R$':
+        valueInR$ = this.inputValue;
+        break;
+      case 'US$':
+        valueInR$ = this.inputValue * 5.07;
+        break;
+      case '€':
+        valueInR$ = this.inputValue * 5.54;
+        break;
+    }
+
+    switch(this.toUnit) {
+      case 'R$':
+        this.convertedValue = valueInR$;
+        break;
+      case 'US$':
+        this.convertedValue = valueInR$ / 5.07;
+        break;
+      case '€':
+        this.convertedValue = valueInR$ / 5.54;
+        break;
+    }
+  }
 }
+

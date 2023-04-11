fb9c367c8e034e27d1c4738b3a7a709398842684
MarianaRosa101 <marianarosa100@gmail.com>
2023-04-11 08:41:10 -0300

    first commit

---
+++ src/app/moeda/moeda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moeda',
+  templateUrl: './moeda.component.html',
+  styleUrls: ['./moeda.component.css']
+})
+export class MoedaComponent {
+
+}
